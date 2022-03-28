e45a8a55579dd7ffb8a5e080a0682718e8c91dc7
Mehedi Hasan <85538308+mehedihasan03@users.noreply.github.com>
2022-03-29 00:18:24 +0600

    Update RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,13 +1,53 @@
 import java.util.Arrays;
 public class RemoveElement{
-	public static void main(String[] args){
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int[] arr = {4,5,6,8,7,2};
 		System.out.println(Arrays.toString(arr));
 		
-		int index1 = 2;
+		System.out.println(arr.length);
 		
-		int[] arrNew = int[arr.length-1];
+		int index = 2;
+		
+		arr = removeTheElement(arr, index);
+		
+		System.out.println(Arrays.toString(arr));
 		
 	}
+	
+	
+	// Function to remove the element
+    public static int[] removeTheElement(int[] arr, int index)
+    {
+ 
+        // If the array is empty
+        // or the index is not in array range
+        // return the original array
+        if (arr == null || index < 0
+            || index >= arr.length) {
+ 
+            return arr;
+        }
+ 
+        // Create another array of size one less
+        int[] anotherArray = new int[arr.length - 1];
+ 
+        // Copy the elements except the index
+        // from original array to the other array
+        for (int i = 0, k = 0; i < arr.length; i++) {
+ 
+            // if the index is
+            // the removal element index
+            if (i == index) {
+                continue;
+            }
+ 
+            // if the index is not
+            // the removal element index
+            anotherArray[k++] = arr[i];
+        }
+ 
+        // return the resultant array
+        return anotherArray;
+    }
 }
